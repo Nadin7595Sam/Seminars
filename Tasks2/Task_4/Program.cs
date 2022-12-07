42ef5ebc62c1c0eb1161ff53d3c841713b28ef2e
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-07 21:02:49 +0300

    done task 4

---
+++ Tasks2/Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int max = n;
+int max2 = n;
+
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    
+    if (n > max) 
+    {
+        max2 = max;
+        max = n;
+    }
+    else if (n > max2 && n < max)
+    {
+        max2 = n;
+    }
+}
+
+Console.WriteLine($"Второе максимальное число {max2}");
