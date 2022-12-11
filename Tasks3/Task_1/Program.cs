426845193e35abc5d58316230dac4604d5df198f
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-11 14:52:50 +0300

    add task 1

---
+++ Tasks3/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string? s = Console.ReadLine();
+while(s ?.Length != 5)
+{
+    Console.WriteLine ("Вы ошиблись!\nВведите пятизначное число: ");
+    s = Console.ReadLine();
+}
+if (s[0] == s[4] && s[1] == s[3])
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
