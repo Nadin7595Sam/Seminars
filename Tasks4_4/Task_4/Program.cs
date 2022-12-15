64a7c29f7d3232359b758c6e76f48063518752f0
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-15 17:22:17 +0300

    done task 4

---
+++ Tasks4_4/Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// void
+
+Console.Clear();
+
+/*void test(int a)
+{
+    a+=5;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+test(n);
+Console.WriteLine(n);*/
+
+void test(ref int a, ref int b)
+{
+    a+=5;
+    b+=5;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), b = 10;
+test(ref n, ref b);
+Console.WriteLine(n);
+Console.WriteLine(b);
