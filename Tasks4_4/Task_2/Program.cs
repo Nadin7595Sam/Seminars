f336197046a5145ca1ddf23c55781abb3b104648
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-15 13:22:50 +0300

    done task 2

---
+++ Tasks4_4/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for(int i = 0; i < array.Length; i++)
+{
+    //                                              * (end - begin) + begin            
+    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2); //[0, 1]
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
