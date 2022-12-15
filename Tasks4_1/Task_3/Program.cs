649cbac39e7213c3debdc677e3a21c082ea05138
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-15 20:48:43 +0300

    done task 3

---
+++ Tasks4_1/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая спрашивает у пользователя кол-во элементов и спршивает сами элементы. После записи элементов в массив, необходимо его вывести на экран.
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент {i}: ");    
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
