148f4624b189ccf3d81c47e4c5cd27809cfee927
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-15 16:17:24 +0300

    done task 3

---
+++ Tasks4_4/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Функции
+// Необходимо вывести наибольшее число из 2-х
+
+Console.Clear();
+
+char test(int a, int b)
+{
+    if(a > b)
+        return '>';
+    return '<';
+}
+
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(test(n, m));
+
+// 1. return
+//         * Возвращает аргумент туда, где была вызвана функция
+//         * Завершает работу функции
+//         * Возвращаемое значение необходимо принимать в консоль (вывод) или в переменную нужного типа данных
+//         * Сколько аргументов принимаем, столько и передаём
