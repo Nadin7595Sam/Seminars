97c44fde995242ef681324deccb8570eac3b4711
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-22 12:07:45 +0300

    done task 1

---
+++ Tasks6_1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	    array[i] = new Random().Next(-100, 1000);
+}
+	
+void ReleaseArray(int[] array)
+{
+	int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+	{
+	    if (array[i] > 0) sum++;
+	}
+    Console.WriteLine(sum);
+}
+	
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
