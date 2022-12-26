9685da9827b98b8d03d203c7ff5864e18027e6dd
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-26 20:49:16 +0300

    done task 1

--- Tasks7_1/Task1/Program.cs
+++ Tasks7_1/Task1/Program.cs
@@ -4,7 +4,7 @@
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 10) - 10, 2); // [-10, 10]
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 10) - 10, 1); // [-10, 10]
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
