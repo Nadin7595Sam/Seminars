5a076e5e3cf8d4aaf67554e92fd78eb04108dfe1
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-12-30 16:42:03 +0300

    done task 1

---
+++ Tasks9/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int rec(int m, int n, int sum)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        sum = sum + m;
+        m++;
+        return rec(m, n, sum);
+    }
+    Console.WriteLine();
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.Write(rec(m, n, sum));
